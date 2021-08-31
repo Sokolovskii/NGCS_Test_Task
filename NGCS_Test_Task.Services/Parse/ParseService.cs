5bd80341f3b1504c2717d91b6f3f01b45b0447da
Sokolovskii Alexander <sokolovskii.alex2@gmail.com>
2021-08-31 21:23:36 +1000

    Исправлена ошибка в условии срабатывании исключения

--- NGCS_Test_Task.Services/Parse/ParseService.cs
+++ NGCS_Test_Task.Services/Parse/ParseService.cs
@@ -9,7 +9,7 @@ namespace NGCS_Test_Task.Services.Parse
 		public AlbumCollection ParseJson(string json)
 		{
 			var parsedJson = JsonConvert.DeserializeObject<AlbumCollection>(json);
-			if(parsedJson.resultCount != 0)
+			if(parsedJson.resultCount == 0)
 			{
 				throw new AlbumsNotFoundException("Альбомы артиста с данным названием не найдены, уточните название и повторите запрос");
 			}
